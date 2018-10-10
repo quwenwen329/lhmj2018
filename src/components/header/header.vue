f844ce78530c31c66f3a6b8da3e2c8a518d5b175
zchq88 <zchq88@aliyun.com>
2018-10-10 16:57:08 +0800

    首页header开发

---
+++ src/components/header/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <div class="banner"></div>
+    <div class="nav-wapper">
+      <div class="nav">
+        <div class="left">
+          <div class="logo2"></div>
+          <div class="line"></div>
+          <div class="logo1"></div>
+        </div>
+        <div class="right">
+          <a>导航</a>
+          <a>导航</a>
+          <a>导航</a>
+          <a>导航</a>
+          <a>导航</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DhHeader',
+};
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "header.styl";
+</style>
--- src/page/index.vue
+++ src/page/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="index">
     <dh-header></dh-header>
-    首页
+    <div>公告</div>
+    <div>领先排行</div>
+    <div>热门榜单</div>
+    <div>大赛概览</div>
+    <div>视讯中心</div>
+    <div>新闻中心</div>
+    <div>历届荣耀</div>
   </div>
 </template>
 
