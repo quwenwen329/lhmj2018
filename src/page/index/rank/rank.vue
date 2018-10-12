d0b5989b482fa1df5edaa77934bae1d9182c0bf9
zchq88 <zchq88@aliyun.com>
2018-10-12 11:28:39 +0800

    领先排行完成

--- src/page/index/rank/rank.vue
+++ src/page/index/rank/rank.vue
@@ -9,6 +9,44 @@
           </span>
           <a href="/" class="more">第十赛季 >></a>
         </div>
+        <div class="military-ranks-wapper">
+          <div v-for="item in militaryRanks" class="military-ranks" :key="item">
+            {{item}}
+          </div>
+        </div>
+        <div class="table">
+          <div class="thead">
+            <div class="thead-td" v-for="item in tableTitles" :key="item">
+              {{item}}
+            </div>
+          </div>
+          <div class="tr" v-for="index1 in [0,1,2,3,4,5,6,7,8]" :key="index1">
+            <div class="td index">
+              <i v-if="index1+1<=3" :class="{ 'rank-1': index1+1==1,
+               'rank-2': index1+1==2,'rank-3': index1+1==3}"></i>
+              <span v-if="index1+1>3">{{index1+1}}</span>
+            </div>
+            <div class="td">选手名称</div>
+            <div class="td star"><i></i></div>
+            <div class="td up">4.4018<i></i></div>
+            <div class="td text-r">1234567890.00</div>
+            <div class="td text-r">0.00</div>
+            <div class="td text-r">0.00</div>
+            <div class="td text-r">00.00%</div>
+            <div class="td">五星上将</div>
+          </div>
+          <div class="tr">
+            <div class="td index">10</div>
+            <div class="td">选手名称</div>
+            <div class="td star"><i></i><i></i><i></i></div>
+            <div class="td down">0.4018<i></i></div>
+            <div class="td text-r">1234567890.00</div>
+            <div class="td text-r">0.00</div>
+            <div class="td text-r">0.00</div>
+            <div class="td text-r">00.00%</div>
+            <div class="td">五星上将</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -17,6 +55,16 @@
 <script>
 export default {
   name: 'DhRank',
+  data() {
+    return {
+      militaryRanks: ['综合评级', '全部选手', '基金组', '集团军', '导弹部队',
+        '空军', '海军', '预备役', '机枪手', '远征军', '盟军'],
+      tableTitles: ['排名', '选手名称', '综合评级', '本届净值', '盈利额',
+        '仓位', '平仓盈亏比', '交易胜率', '衔级'],
+      data1: ['1000', '选手名称选手名称', '综合评级综合评级综合评级', '1.0000', '12345678901234.00',
+        '0.12', '1.66', '56.56%', '五星上将'],
+    };
+  },
 };
 </script>
 
