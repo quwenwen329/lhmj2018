0916172365d579bc198495ea39c9554a21c83721
zchq88 <zchq88@aliyun.com>
2018-10-12 13:42:34 +0800

    热门榜单样式完成

---
+++ src/page/index/hotList/hotList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hot-list">
+    <div class="title-wapper"><i class="title"></i></div>
+    <div class="list">
+      <div class="item" v-for="item in [0,1,2,3,4]" :key="item">
+        <div class="title">日赚净值</div>
+        <div class="text" :class="{'down':item==3}">5.2342</div>
+        <div class="icon">{{item+1}}</div>
+        <div class="military-rank">五星上将</div>
+        <div class="ID">老男孩</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DhHotList',
+};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  @import "hotList.styl";
+</style>
--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -3,7 +3,7 @@
     <dh-header>顶部导航</dh-header>
     <dh-notice>公告</dh-notice>
     <dh-rank>领先排行</dh-rank>
-    <div>热门榜单</div>
+    <dh-hot-list>热门榜单</dh-hot-list>
     <div>大赛概览</div>
     <div>视讯中心</div>
     <div>新闻中心</div>
@@ -14,12 +14,14 @@
 <script>
 import DhNotice from './notice/notice';
 import DhRank from './rank/rank';
+import DhHotList from './hotList/hotList';
 
 export default {
   name: 'Index',
   components: {
     DhNotice,
     DhRank,
+    DhHotList,
   },
 };
 </script>
