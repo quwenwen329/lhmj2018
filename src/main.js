33db7dbc56a9c46e1a253408e42c0b5fcded718f
zchq88 <zchq88@aliyun.com>
2018-10-15 14:41:13 +0800

    引入vue-awesome-swiper

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import VueAwesomeSwiper from 'vue-awesome-swiper';
+// eslint-disable-next-line
+import 'swiper/dist/css/swiper.css';
 
 import ajax from '@/components/js/ajax';
 
@@ -8,6 +11,8 @@ import DhHeader from '@/components/header/header';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+
+Vue.use(VueAwesomeSwiper/* ,{ default global options } */);
 
 Vue.config.productionTip = false;
 Vue.prototype.ajax = ajax;
